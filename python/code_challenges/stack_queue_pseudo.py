a3016c47755178829c90513748e2e6c2bb0603cc
jeremy-adamson <jadamson.physics@gmail.com>
2023-04-24 20:12:21 -0700

    wonky queue

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,29 @@
 from data_structures.stack import Stack
+from data_structures.stack import Node
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stackmain = Stack()
+        self.stackaux = Stack()
+
+    def enqueue(self, value):
+        self.stackmain.push(value)
+
+        if self.stackaux.is_empty():
+            self.stackaux.push(self.stackmain.top)
+            return
+
+        traverse = self.stackaux.top
+        while traverse.next:
+            traverse = traverse.next
+        traverse.next = Node(self.stackmain.top)
+
+
+    def dequeue(self):
+        if self.stackmain.is_empty():
+            raise Exception("Queue is empty")
+
+        value = self.stackaux.pop().value
+        self.stackaux.top().next = None
+        return value
