60b126369e3c13f386c8c2d7ebd43ac6f69ec3cf
jeremy-adamson <jadamson.physics@gmail.com>
2023-06-26 21:19:42 -0700

    hashtable implementation

--- python/data_structures/hashtable.py
+++ python/data_structures/hashtable.py
@@ -1,12 +1,45 @@
+from mpmath import mp
+
 class Hashtable:
-    """
-    Put docstring here
-    """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.core = [False]*1000
 
-    def some_method(self):
-        # method body here
-        pass
+    def set(self, key, value):
+        index = self.hash(key)
+        self.core[index] = [key, value]
+
+    def get(self, key):
+        index = self.hash(key)
+        value = self.core[index][1]
+        return value
+
+    def has(self, key):
+        index = self.hash(key)
+        key_value = self.core[index]
+        if not key_value:
+            return False
+        elif key_value[0] == key:
+            return True
+        else:
+            return False
+
+    def keys(self):
+        key_list = []
+        for index in range(len(self.core)):
+            if self.core[index]:
+                key_list.append(self.core[index][0])
+        return key_list
+
+
+    def hash(self, key):
+        mp.dps = 100
+        pi = mp.pi
+        summation = 0
+
+        for index, character in enumerate(key):
+            summation+=ord(character)*pi*10**index
+
+        hash_value = int(summation%len(self.core))
+
+        return hash_value
--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -6,7 +6,7 @@ def test_exists():
     assert Hashtable
 
 
-@pytest.mark.skip("TODO")
+#@pytest.mark.skip("TODO")
 def test_get_apple():
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
@@ -32,3 +32,55 @@ def test_internals():
     expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
 
     assert actual == expected
+
+def test_more_fruit():
+    hashtable = Hashtable()
+    hashtable.set("apple", "apple sauce")
+    hashtable.set("grape", "grape juice")
+    hashtable.set("grapefruit", "yummy")
+    hashtable.set("mango", "why would you eat this")
+    actual = hashtable.get("mango")
+    expected = "why would you eat this"
+    assert actual == expected
+
+def test_contains():
+    hashtable = Hashtable()
+    hashtable.set("apple", "apple sauce")
+    hashtable.set("grape", "grape juice")
+    hashtable.set("grapefruit", "yummy")
+    hashtable.set("mango", "why would you eat this")
+    actual = hashtable.has("grape")
+    expected = True
+    assert actual == expected
+
+def test_contains2():
+    hashtable = Hashtable()
+    hashtable.set("apple", "apple sauce")
+    hashtable.set("grape", "grape juice")
+    hashtable.set("grapefruit", "yummy")
+    hashtable.set("mango", "why would you eat this")
+    actual = hashtable.has("dog")
+    expected = False
+    assert actual == expected
+
+def test_keys():
+    hashtable = Hashtable()
+    hashtable.set("apple", "apple sauce")
+    hashtable.set("grape", "grape juice")
+    hashtable.set("grapefruit", "yummy")
+    hashtable.set("mango", "why would you eat this")
+    actual = hashtable.keys()
+    expected = ["apple", "grape", "grapefruit", "mango"]
+    assert set(actual) == set(expected)
+
+def test_keys2():
+    hashtable = Hashtable()
+    hashtable.set("apple", "apple sauce")
+    hashtable.set("grape", "grape juice")
+    hashtable.set("grapefruit", "yummy")
+    hashtable.set("mango", "why would you eat this")
+    actual = hashtable.keys()
+    expected = ["apple", "grape", "grapefruit", "mango", "dragonfruit"]
+    assert set(actual) != set(expected)
+
+
