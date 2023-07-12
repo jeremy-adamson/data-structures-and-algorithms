fdec51db650a721df9a8502c820cbc5abb9ca4a3
jeremy-adamson <jadamson.physics@gmail.com>
2023-07-11 20:13:57 -0700

    hashtable repeat word complete

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,21 @@
 from data_structures.hashtable import Hashtable
+import re
+
+def first_repeated_word(input_string):
+    collection = Hashtable()
+    removed_punctuation = remove_punctuation(input_string)
+    parsed_input = removed_punctuation.split()
+
+    for word in parsed_input:
+        word = word.lower()
+        if collection.has(word):
+            return word
+        collection.set(word, True)
+
+    return None
 
 
-def first_repeated_word():
-    pass
+def remove_punctuation(input_string):
+    pattern = r'[^\w\s]'
+    filtered = re.sub(pattern, '', input_string)
+    return filtered
